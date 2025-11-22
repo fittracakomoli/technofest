a234417bfd762d36f611ebd425aa4ca6e3b42026
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2025-11-22 16:20:20 +0700

    feat: install laravel sanctum

---
+++ api-backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/status', function () {
+    return response()->json(['status' => 'API is running']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
